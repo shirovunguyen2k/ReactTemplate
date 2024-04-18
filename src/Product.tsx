3fc4cd6b479f0ac1c7736a0545324fce35bf7ca3
TramPham171 <tram170103@gmail.com>
2024-04-18 10:39:42 +0700

    feat: sold out

--- src/Product.tsx
+++ src/Product.tsx
@@ -5,7 +5,8 @@ import { v4 as uuidv4 } from "uuid";
 const listProduct: IProduct[] = [
   {
     id: uuidv4(),
-    image: "https://product.hstatic.net/1000230642/product/hsw002900kem__10__730d39626a494798aae0d2911879d67b.jpg",
+    image:
+      "https://product.hstatic.net/1000230642/product/hsw002900kem__10__730d39626a494798aae0d2911879d67b.jpg",
     title: "Lorem Ipsum",
     description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit",
     price: 45,
@@ -13,11 +14,21 @@ const listProduct: IProduct[] = [
   },
   {
     id: uuidv4(),
-    image: "https://product.hstatic.net/1000230642/product/hsw002900hog__2__9ab53305f6e448e69e9c5cb63269bc1f.jpg",
+    image:
+      "https://product.hstatic.net/1000230642/product/hsw002900hog__2__9ab53305f6e448e69e9c5cb63269bc1f.jpg",
     title: "Lorem Ipsum",
     description: "Lorem ipsum dolor sit, ramet consectetur adipisicing elit",
     price: 45,
     amount: 2,
+  },
+  {
+    id: uuidv4(),
+    image:
+      "https://product.hstatic.net/1000230642/product/hsw002900hog__2__9ab53305f6e448e69e9c5cb63269bc1f.jpg",
+    title: "Lorem Ipsum",
+    description: "Lorem ipsum dolor sit, ramet consectetur adipisicing elit",
+    price: 45,
+    amount: 8,
   },
 ];
 const Product = () => {
--- src/ProductCard.tsx
+++ src/ProductCard.tsx
@@ -12,7 +12,9 @@ const ProductCard = (props: ProductCardProps) => {
         <img src={product.image} />
       </div>
       <div className="p-7 text-center">
-        <h1 className="text-2xl text-orange-800 font-bold py-3">{product.title}</h1>
+        <h1 className="text-2xl text-orange-800 font-bold py-3">
+          {product.title}
+        </h1>
         <p className="text-orange-800">{product.description}</p>
       </div>
       <div className="flex justify-center">
@@ -20,6 +22,9 @@ const ProductCard = (props: ProductCardProps) => {
         <button className="bg-blue-500 w-6 h-6 rounded-full mx-2"></button>
         <button className="bg-yellow-600 w-6 h-6 rounded-full mx-2"></button>
       </div>
+      <span className="flex justify-center pt-3">
+        {product.amount > 0 ? product.amount : "Sold out"}
+      </span>
       <div className="flex justify-between p-5 font-bold">
         <span className="p-2 text-orange-800 text-xl">${product.price},00</span>
         <button className="outline outline-1 outline-orange-600 p-2 px-9 rounded-lg text-orange-800">
